1438c55c66a52409d8dfd2d8dd19cf11afc7a703
Benatovich <sambenatovich@gmail.com>
2021-12-16 14:58:38 -0500

    render-utils lookin good

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-// import functions and grab DOM elements
+import {
+    redirectToPolls,
+    signInUser,
+    signUpUser,
+} from './fetch-utils.js';
 
-// let state
+const signInForm = document.getElementById('sign-in');
+const signInEmail = document.getElementById('sign-in-email');
+const signInPassword = document.getElementById('sign-in-password');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const signUpForm = document.getElementById('sign-up');
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('sign-up-password');
+
+redirectToPolls();
+
+signUpForm.addEventListener('submit', async(event)=>{
+    event.preventDefault();
+    const user = await signUpUser(signUpEmail.ariaValueMax, signUpPassword.value);
+
+    if (user){
+        redirectToPolls();
+    } else {
+        console.error(user);
+    }
+});
+
+signInForm.addEventListener('submit', async(event)=>{
+    event.preventDefault();
+    const user = await signInUser(signInEmail.value, signInPassword.value);
+
+    if (user){
+        redirectToPolls();
+    } else {
+        console.error(user);
+    }
+});
---
+++ render-utils.js
@@ -0,0 +1,57 @@
+let optionATitle = '';
+let optionBTitle = '';
+let optionAVotes = 0;
+let optionBVotes = 0;
+
+export function renderOption(title, votes) {
+    const container = document.createElement('div');
+    const titleEl = document.createElement('p');
+    const votesEl = document.createElement('p');
+    
+    container.classList.add('option');
+    titleEl.textContent = title;
+    votesEl.textContent = votes;
+    
+    container.append(titleEl, votesEl);
+    
+    return container;
+}
+
+export function renderPoll(poll) {
+    const container = document.createElement('div');
+    const questionEl = document.createElement('p');
+    const optionADiv = document.createElement('div');
+    const optionBDiv = document.createElement('div');
+    
+    container.classList.add('poll');
+    questionEl.textContent = poll.question;
+    optionADiv.textContent = renderOption(optionATitle, optionAVotes);
+    optionBDiv.textContent = renderOption(optionBTitle, optionBVotes);
+
+    container.append(questionEl, optionADiv, optionBDiv);
+
+    return container;
+}
+
+// renderPastPoll(pastPoll) function?
+// better safe than sorry
+
+export function renderPastPoll(pastPoll) {
+    const container = document.createElement('div');
+    const pQuestionEl = document.createElement('p');
+    const pTitleA = document.createElement('p');
+    const pTitleB = document.createElement('p');
+    const pVotesA = document.createElement('p');
+    const pVotesB = document.createElement('p');
+
+    container.classList.add('past-poll');
+    pQuestionEl.textContent = pastPoll.question;
+    pTitleA.textContent = pastPoll.optionATitle;
+    pTitleB.textContent = pastPoll.optionBTitle;
+    pVotesA.textContent = pastPoll.optionAVotes;
+    pVotesB.textContent = pastPoll.optionBVotes;
+
+    container.append(pQuestionEl, pTitleA, pTitleB, pVotesA, pVotesB);
+
+    return container;
+}
